1f36e9fcb0028af1660067e0692cdca9af3ec3f0
Cryp70-C0d3r <cryp70@tuta.io>
2021-02-25 00:07:40 -0600

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   # Set Project Static Page Directories #
-  get '/about',          to: 'static_pages#about'
-  get '/contact',        to: 'static_pages#contact'
-  get '/services',       to: 'static_pages#services'
-  get '/portfolio',      to: 'static_pages#portfolio'
+  get 'about',          to: 'static_pages#about'
+  get 'contact',        to: 'static_pages#contact'
+  get 'services',       to: 'static_pages#services'
+  get 'portfolio',      to: 'static_pages#portfolio'
 
   # Set Project Resources #
   resources :contact, only: [:index, :new, :create]
